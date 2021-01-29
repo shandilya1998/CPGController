4f868513af51965f62adc9143a6471d4e61ce709
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-01-29 14:36:57 +0530

    commit planner working

--- src/simulations/ws/src/quadruped/scripts/kinematics.py
+++ src/simulations/ws/src/quadruped/scripts/kinematics.py
@@ -4,6 +4,9 @@ from moveit_msgs.msg import RobotState
 from moveit_msgs.srv import GetPositionFK, GetPositionFKRequest
 from std_msgs.msg import Header
 
+# refer to the following link for creation of a service to get fk
+# https://groups.google.com/g/moveit-users/c/Wb7TqHuf-ig
+
 class Kinematics:
     def __init__(self, params):
         self.params = params
