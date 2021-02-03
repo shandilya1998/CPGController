a45bce1642d4bd20832fb8e5b590ec26bff8dfbe
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-02-03 14:29:23 +0530

    commit force torque sensor functional stability reward calculation done energy reward computation to be set up
    Former-commit-id: 28c05f83b7aebb3d5633ced2a276b7b63d7e6eb0

--- src/reward/zmp.py
+++ src/reward/zmp.py
@@ -18,6 +18,8 @@ class ZMP:
         ])
         self.zmp_s = np.zeros((3,))
         self.zmp = np.zeros((3,))
+        self.inertial_plane = np.eye(N = 3, k = 1)
+        self.support_plane = self.inertial_plane
 
     def update_g(self):
         g = self.get_physics_prop_proxy().gravity
@@ -30,13 +32,15 @@ class ZMP:
     def build(self, t, Tb, A, B, AL, BL, AF, BF):
         self.support_plane.build(t, Tb, A, B, AL, BL, AF, BF)
 
+    def _transform(self, vec, cs1, cs2):
+            return self.support_plane.transform(vec, cs1, cs2)
+
     def get_ZMP_s(self, com, force, torque):
-        plane = self.support_plane()
-        inertial_plane = np.eye(N = 3, k = 1)
-        com_s = transform(com, plane, inertial_plane)
-        force_s = transform(force, plane, inertial_plane)
-        torque_s = transform(torque, plane, inertial_plane)
-        g_s = transform(self.g, plane, inertial_plane)
+        self.plane = self.support_plane()
+        com_s = self._transform(com, plane, self.inertial_plane)
+        force_s = self._transform(force, plane, self.inertial_plane)
+        torque_s = self._transform(torque, plane, self.inertial_plane)
+        g_s = self._transform(self.g, plane, self.inertial_plane)
         zmp_s = np.zeros((3,))
         zmp_s[1] = com_s[1] - (
             com_s[0] * (
@@ -54,4 +58,4 @@ class ZMP:
         self.zmp_s = self.get_ZMP_s(com, force, torque)
         self.zmp = self.zmp_s + eta*(v_real - v_exp)
         self.zmp[0] = 0
-        return self.zmp
+        return self._transform(self.zmp, self.inertial_plane, self.plane)
