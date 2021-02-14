d29784f614ddd52b224586fe19009f9d820f635e
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-02-14 12:41:20 +0530

    commt setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,19 @@ source venv/bin/activate
 pip install -r requirements.txt
 apt-get update
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
+curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | apt-key add -
 apt update
-apt install ros-noetic-desktop-full
-echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+apt install ros-melodic-desktop-full
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/shandilya/Desktop/CNS/DDP/src/simulations/ws/src
+apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+apt install python-rosdep
+rosdep init
+rosdep update
+cd src/simulations/ws
+sed -i 's#/home/shandilya/Desktop/CNS/DDP#/content/CPGController#g' build/CMakeCache.txt
+sed -i 's#/home/shandilya/Desktop/CNS/DDP#/content/CPGController#g' build/Makefile
+catkin_make
+source devel/setup.bash
+cd .. && cd ..
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/content/CPGController/src/simulations/ws/src
