07f1e06188279114703115c757814f746c43da02
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-02-14 14:27:51 +0530

    commit colab setup for training
    Former-commit-id: cc59aef2050ecf86c31ea11ed01142cd91b96519

--- setup.sh
+++ setup.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-pip3 install virtualenv
-python3 -m virtualenv venv
-source venv/bin/activate
+update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+update-alternatives --install /usr/local/bin/python python /usr/bin/python 1
+python -m pip install --upgrade --force pip
 pip install -r requirements.txt
-apt-get update
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | apt-key add -
-apt update
-apt install ros-melodic-desktop-full
+sudo apt-get update
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
+sudo apt update
+sudo apt install ros-melodic-desktop-full
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
-apt install python-rosdep
-rosdep init
+sudo apt install python-rosdep python-catkin-pkg python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo rosdep init
 rosdep update
 cd src/simulations/ws
 sed -i 's#/home/shandilya/Desktop/CNS/DDP#/content/CPGController#g' build/CMakeCache.txt
