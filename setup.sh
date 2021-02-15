9fa8eb511d2f05bcf12118ce6d5090235a732d30
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-02-15 11:11:39 +0530

    commit setup.shh functional, new functionality added to be tested
    Former-commit-id: 1085ac97c91427fff17c862c2f527791a2146c27

--- setup.sh
+++ setup.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
-update-alternatives --install /usr/bin/python python /usr/bin/python3 1
-update-alternatives --install /usr/local/bin/python python /usr/bin/python 1
-python -m pip install --upgrade --force pip
-pip install -r requirements.txt
-sudo apt-get update
+echo Starting ROS install
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
+
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
 sudo apt update
+
 sudo apt install ros-melodic-desktop-full
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
+sudo apt install -y ros-melodic-rosmaster
+
+pip install --extra-index-url https://rospypi.github.io/simple/ rospy
 sudo apt install python-rosdep python-catkin-pkg python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo rosdep init
 rosdep update
-cd src/simulations/ws
+
+echo ROS install done
+
+printf -- '#!/bin/bash\n' > ros_start.sh
+printf -- 'source /opt/ros/melodic/setup.bash\n' >> ros_start.sh
+printf -- '/opt/ros/melodic/bin/roscore' >> ros_start.sh
+chmod +x ros_start.sh
+#cat ros_start.sh
+
+echo Setting up Quadruped package
+cd CPGController/src/simulations/ws
 sed -i 's#/home/shandilya/Desktop/CNS/DDP#/content/CPGController#g' build/CMakeCache.txt
 sed -i 's#/home/shandilya/Desktop/CNS/DDP#/content/CPGController#g' build/Makefile
 catkin_make
 source devel/setup.bash
-cd .. && cd ..
+cd ../../../
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/content/CPGController/src/simulations/ws/src
+echo Package setup done
