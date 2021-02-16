8542ec48bbf7be30ca3ffe2c507f9d51ddd194d1
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-02-16 12:12:12 +0530

    sync

--- src/simulations/ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shandilya/Desktop/CNS/DDP/src/simulations/ws/src
-# Build directory: /home/shandilya/Desktop/CNS/DDP/src/simulations/ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("quadruped")
-subdirs("mebot/quadruped_description")
--- src/simulations/ws/build/quadruped/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shandilya/Desktop/CNS/DDP/src/simulations/ws/src/quadruped
-# Build directory: /home/shandilya/Desktop/CNS/DDP/src/simulations/ws/build/quadruped
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
