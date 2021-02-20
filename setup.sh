f916b449dca8f3262c745d93cec8c41838e9006f
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-02-20 15:05:35 +0530

    commit setup.sh
    Former-commit-id: 99189516bc32e501e864cef3334e6c51417d1221

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt install linux-headers-$(uname -r)
+curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+sudo apt update
+sudo apt -y install cuda
+sudo nvidia-smi
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt update
+sudo apt -y install ros-noetic-desktop-full
+source /opt/ros/noetic/setup.bash
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt -y install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt -y install python3-rosdep
+sudo rosdep init
+rosdep update
+sudo apt -y install ros-noetic-moveit
