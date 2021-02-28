ed8be50b9faddc9924dc0c4fb989fca8720a0b60
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-02-28 11:11:33 +0530

    commit sync

--- src/run.py
+++
@@ -1,5 +0,0 @@
-from simulations.ws.src.quadruped_control import robot
-from rl.constants import params
-
-spider = robot.WalkingSpider(params)
-robotID = spider._load_urdf('simulations/ws/src/quadruped_description/urdf/spider_simple.urdf')
