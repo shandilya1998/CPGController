4c014d1a201ec0dfb149e617fbfc27dcda41402f
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-04-05 17:21:28 +0530

    commit plots
    Former-commit-id: 4e0fd6f28c07673e9192b0b2e52aa2969aba4ddb

--- src/test_data.py
+++ src/test_data.py
@@ -10,20 +10,17 @@ Y = np.load(os.path.join(f, 'Y.npy'))
 MU = np.load(os.path.join(f, 'MEAN.npy'))
 MEAN = np.load(os.path.join(f, 'MU.npy'))
 
-num = Y.shape[0]
-time = np.arange(Y.shape[1])
+steps = Y.shape[1]
 
-for j in tqdm(range(0, num, 6000)):
-    y = Y[j]
-    steps = y.shape[0]
-    mean = MEAN[j]
-    mu = MU[j]
-    y = y * mu * np.pi / 3 + mean
+y = Y * np.repeat(np.expand_dims(MU * np.pi/3, 1), steps, 1) + np.repeat(np.expand_dims(MEAN, 1), steps, 1)
+num = Y.shape[0]
+
+for j in tqdm(range(0, num, 1000)):
     fig, ax = plt.subplots(4,1, figsize = (5,20))
     for i in range(4):
-        ax[i].plot(time, y[:, 3 * i], 'g', label = 'ankle')
-        ax[i].plot(time, y[:, 3 * i + 1], 'b', label = 'knee')
-        ax[i].plot(time, y[:, 3 * i + 2], 'r', label = 'hip')
+        ax[i].plot(y[j][:, 3 * i], 'g', label = 'ankle')
+        ax[i].plot(y[j][:, 3 * i + 1], 'b', label = 'knee')
+        ax[i].plot(y[j][:, 3 * i + 2], 'r', label = 'hip')
         ax[i].legend()
     fig.savefig(os.path.join(f, path, 'plot_{j}.png'.format(j = j)))
     plt.close()
