ce4c793e12f2f2d8abb518f15d80cb8888f88468
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-04-05 21:43:23 +0000

    commit exp25 pretraining exp7 rl
    Former-commit-id: 08fb70c56840eb909d200845dc9642e693e8cf45

--- src/rl/env.py
+++ src/rl/env.py
@@ -89,10 +89,10 @@ class Env(tfa.environments.tf_environment.TFEnvironment):
                 _action,
                 desired_motion
             )
-            reward += 0.008 * self.quadruped.get_COT()
+            reward += 1e-3 * self.quadruped.get_COT()
             reward += self.quadruped.get_motion_reward()
-        #reward += 0.1 * self.quadruped.get_stability_reward()
-        reward += self.quadruped.reward
+        reward += 1e-5 * self.quadruped.get_stability_reward()
+        #reward += self.quadruped.reward
         action[0] = swap_batch_timestep(action[0])
         action[1] = swap_batch_timestep(action[1])
         observation = [
