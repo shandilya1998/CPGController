b4cfd3d300fcd231e5a8945ff78bd6555118fe67
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-04-08 18:14:25 +0000

    commit trainingining results
    Former-commit-id: 9039812ef8c4b49e57eabda70e7690e39440201a

--- src/learn.sh
+++ src/learn.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-export ROS_MASTER_URI=http://localhost:11350
-export GAZEBO_MASTER_URI=http://localhost:11340
+export ROS_MASTER_URI=http://localhost:11355
+export GAZEBO_MASTER_URI=http://localhost:11345
 
 OUT_PATH="rl/out_dir/models"
-EXPERIMENT=9
-START=150
+EXPERIMENT=10
+START=0
 
 nohup roslaunch quadruped quadruped_control.launch \
     >> $OUT_PATH/exp$EXPERIMENT/ros.log &
