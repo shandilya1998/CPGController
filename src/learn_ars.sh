d6cf94003be6d0b89a1f871d00e4f18fcabf2db8
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-04-08 08:09:38 +0530

    commit ars functional need to run on gpu now
    Former-commit-id: 5c953506b4c43777b5a977246334e33f9371156e

---
+++ src/learn_ars.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export ROS_MASTER_URI=http://localhost:11351
+export GAZEBO_MASTER_URI=http://localhost:11341
+
+OUT_PATH="rl/out_dir/models/ars"
+EXPERIMENT=1
+
+nohup roslaunch quadruped quadruped_control.launch \
+    >> $OUT_PATH/exp$EXPERIMENT/ros.log &
+nohup python3 -u ars.py \
+    --experiment $EXPERIMENT \
+    --out_path $OUT_PATH \
+    >> $OUT_PATH/exp$EXPERIMENT/learn.log &
