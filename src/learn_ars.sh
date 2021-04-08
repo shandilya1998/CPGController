130fc235d61f718eeb6f98c42c88ed7e841fc18e
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-04-08 17:56:37 +0000

    commit experiments out
    Former-commit-id: 4f8a7f7360ed4643c532960c467aa7a0673f5ce0

--- src/learn_ars.sh
+++ src/learn_ars.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-export ROS_MASTER_URI=http://localhost:11351
-export GAZEBO_MASTER_URI=http://localhost:11341
+export ROS_MASTER_URI=http://localhost:11352
+export GAZEBO_MASTER_URI=http://localhost:11342
 
 OUT_PATH="rl/out_dir/models/ars"
-EXPERIMENT=2
-START=75
+EXPERIMENT=1
+START=200
 
 nohup roslaunch quadruped quadruped_control.launch \
     >> $OUT_PATH/exp$EXPERIMENT/ros.log &
 nohup python3 -u ars.py \
     --experiment $EXPERIMENT \
     --out_path $OUT_PATH \
+    --start $START \
     >> $OUT_PATH/exp$EXPERIMENT/learn.log &
