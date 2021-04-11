fa1ff1cb9ea20a9017ff7a303f96f4795c6b1135
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-04-11 11:31:29 +0000

    commit ddpg exp 14 ars exp5
    Former-commit-id: 40b8f8c260fbfd8bc7be6601613280ee9782a01f

--- src/learn_ars.sh
+++ src/learn_ars.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-export ROS_MASTER_URI=http://localhost:11313
-export GAZEBO_MASTER_URI=http://localhost:11314
+export ROS_MASTER_URI=http://localhost:11317
+export GAZEBO_MASTER_URI=http://localhost:11318
 
 OUT_PATH="rl/out_dir/models/ars"
-EXPERIMENT=4
+EXPERIMENT=5
 START=0
 
 nohup roslaunch quadruped quadruped_control.launch \
