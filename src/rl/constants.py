294197bb6c2907d419884c81e9c5fc32f87045c7
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-04-19 09:18:23 +0000

    commit plots
    Former-commit-id: 90e042b52eac2ba17562292932a9ff42478641fe

--- src/rl/constants.py
+++ src/rl/constants.py
@@ -36,7 +36,7 @@ params = {
     'action_dim'                  : action_dim,
 
     'units_action_input'          : 20,
-    'rnn_steps'                   : 10,
+    'rnn_steps'                   : 50,
     'units_critic_hidden'         : 20,
     'lstm_units'                  : action_dim,
     'lstm_state_dense_activation' : 'relu',
