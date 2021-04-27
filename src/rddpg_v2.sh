6bb9446d7116c0ed3b9ed40ae852ade3f3c2b9d4
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-04-27 16:07:02 +0000

    commit td actor pretrain data gen code
    Former-commit-id: e1a6bd5b9895aeeaaa6449aceac3726035351343

--- src/rddpg_v2.sh
+++ src/rddpg_v2.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-export ROS_MASTER_URI=http://localhost:11311
-export GAZEBO_MASTER_URI=http://localhost:11312
+export ROS_MASTER_URI=http://localhost:11315
+export GAZEBO_MASTER_URI=http://localhost:11316
 
-OUT_PATH="rl/out_dir/models"
-EXPERIMENT=36
+OUT_PATH="weights/actor_pretrain"
+EXPERIMENT=53
 START=0
 PER="false"
 HER="false"
