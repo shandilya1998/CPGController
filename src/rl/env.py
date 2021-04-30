b79485515185231ac24d613a30eddd3a88efc67d
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-05-01 00:54:16 +0530

    commit plots exp38

--- src/rl/env.py
+++ src/rl/env.py
@@ -100,7 +100,7 @@ class Env(tfa.environments.tf_environment.TFEnvironment):
             _action,
             desired_motion
         )
-        #self.COT +=  0.002 * self.quadruped.get_COT()
+        self.COT +=  0.002 * self.quadruped.get_COT()
         self.r_motion += self.quadruped.get_motion_reward()
         self.quadruped.set_support_lines()
         self.stability += 0.002 * self.quadruped.get_stability_reward()
