afe3ee1dd02adf615b213c903647933e340ff9b8
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-05-05 14:45:04 +0000

    commit plots

--- src/rddpg_torch.sh
+++ src/rddpg_torch.sh
@@ -1,8 +1,8 @@
-export ROS_MASTER_URI=http://localhost:11311
-export GAZEBO_MASTER_URI=http://localhost:11312
+export ROS_MASTER_URI=http://localhost:11313
+export GAZEBO_MASTER_URI=http://localhost:11314
 
 OUT_PATH="rl/out_dir/models"
-EXPERIMENT=45
+EXPERIMENT=46
 
 nohup roslaunch quadruped quadruped_control.launch \
     >> $OUT_PATH/exp$EXPERIMENT/ros.log &
